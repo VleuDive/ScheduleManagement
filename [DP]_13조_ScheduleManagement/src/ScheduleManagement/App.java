8c9be1ce0cd9d1c84c1b71a71c8cbf9eb8151b1a
VleuDive <hot1999130@gmail.com>
2017-11-02 10:31:23 +0900

    One GUI Class removed

---
+++ [DP]_13조_ScheduleManagement/src/ScheduleManagement/App.java
@@ -0,0 +1,60 @@
+package ScheduleManagement;
+
+public class App {
+	private int p_User;
+	private Daily_Schedule DailyPointer;
+	private Weekly_Schedule WeeklyPointer;
+	private Monthly_Schedule MonthlyPointer;
+	private Goal S_Goal;//배열 또는 List로 
+	private BucketList S_BucketList;//배열 또는 List로 
+	
+	public int getUser()
+	{
+		return p_User;
+	}
+	public Daily_Schedule getDPointer()
+	{
+		return DailyPointer;
+	}
+	public Weekly_Schedule getWPointer()
+	{
+		return WeeklyPointer;
+	}
+	public Monthly_Schedule getMPointer()
+	{
+		return MonthlyPointer;
+	}
+	public Goal getGoal()
+	{
+		return S_Goal;
+	}
+	public BucketList getBucketList()
+	{
+		return S_BucketList;
+	}
+	
+	public void setPUser(int p)
+	{
+		p_User = p;
+	}
+	public void setDailyPointer(Daily_Schedule dailyPointer) 
+	{
+		DailyPointer = dailyPointer;
+	}
+	public void setWeeklyPointer(Weekly_Schedule weeklyPointer)
+	{
+		WeeklyPointer = weeklyPointer;
+	}
+	public void setMonthlyPointer(Monthly_Schedule monthlyPointer) 
+	{
+		MonthlyPointer = monthlyPointer;
+	}
+	public void setSGoal(Goal s_Goal)
+	{
+		S_Goal = s_Goal;
+	}
+	public void setSBucketList(BucketList s_BucketList)
+	{
+		S_BucketList = s_BucketList;
+	}
+}
--- [DP]_13조_ScheduleManagement/src/ScheduleManagement/DailyGoal.java
+++ [DP]_13조_ScheduleManagement/src/ScheduleManagement/DailyGoal.java
@@ -4,7 +4,7 @@ public class DailyGoal {
 
 	private String Name;
 	private boolean CheckBK;
-	
+	//Daily goal 할당도 필요함. 날짜 이월을 자연스럽게 처리할 알고리즘 필요.
 	public String getName()
 	{
 		return Name;
